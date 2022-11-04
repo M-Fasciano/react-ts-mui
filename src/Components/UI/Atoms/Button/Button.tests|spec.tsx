07793e65be9e4268193bbd87d125536749297769
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2022-11-04 14:26:19 +0000

    Button

---
+++ src/Components/UI/Atoms/Button/Button.tests|spec.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Button from "./Button";
+
+describe("Button Component Tests", () => {
+  it("Renders correctly in DOM", () => {
+    <Button label={"Hello world"} />;
+  });
+});
