668d8f988eed501e37cf058ecdd3fd6de31beca6
michele fasciano <michelefasciano37@hotmail.co.uk>
2022-12-10 14:08:16 +0000

    React player wistia

---
+++ src/Components/WistiaPlayerEmbed/WistiaPlayerEmbed.styles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledVideoWrapper = styled("div")`
+  padding-top: 56.25%;
+  position: relative;
+  width: 100%;
+
+  .react-player {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+  }
+`;
