f72620c6b10a3394282f4f3d6bb1de38ba68af3b
michele fasciano <michelefasciano37@hotmail.co.uk>
2022-12-31 08:15:11 +0100

    Updated: Visual comparisons

--- src/components/molecules/Navigation/Navigation.spec.tsx
+++ src/components/molecules/Navigation/Navigation.spec.tsx
@@ -4,16 +4,25 @@ test("navigation login", async ({ page }) => {
   await page.goto("http://localhost:3000/login");
   // Expects the URL to contain login.
   await expect(page).toHaveURL("http://localhost:3000/login");
+
+  // On first execution, Playwright test will generate reference screenshots.
+  await expect(page).toHaveScreenshot();
 });
 
 test("navigation video", async ({ page }) => {
   await page.goto("http://localhost:3000/video");
   // Expects the URL to contain video.
   await expect(page).toHaveURL("http://localhost:3000/video");
+
+  // On first execution, Playwright test will generate reference screenshots.
+  await expect(page).toHaveScreenshot();
 });
 
 test("navigation layout", async ({ page }) => {
   await page.goto("http://localhost:3000/layout");
   // Expects the URL to contain layout.
   await expect(page).toHaveURL("http://localhost:3000/layout");
+
+  // On first execution, Playwright test will generate reference screenshots.
+  await expect(page).toHaveScreenshot();
 });
