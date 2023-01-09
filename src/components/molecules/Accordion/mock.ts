52d1677357d1189a77085040739bd0eec367c97c
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-01-09 08:55:05 +0000

    Updated: added style to accordion

---
+++ src/components/molecules/Accordion/mock.ts
@@ -0,0 +1,26 @@
+export const faqs = [
+  {
+    id: 'panel1',
+    title: 'How much will delivery cost?',
+    content: `Delivery is absolutely FREE on orders of £59 and above - if your order
+    is below that amount, we charge £3.98. It's also super-speedy: simply
+    order by 11:45pm (UK) and receive your contacts the very next day.`,
+  },
+  {
+    id: 'panel2',
+    title: 'How many promo codes can I use at the same time?',
+    content: `You can use 1 promo code per transaction! Simply copy the code you'd
+    like to use and paste it at the dedicated basket field, when you’re
+    ready to check out. Please note that our amazing policies such as Free
+    lens replacement or 365-day guarantee are available with every
+    purchase so they don't count as a promotion.`,
+  },
+  {
+    id: 'panel3',
+    title: 'How can I get in touch with you?',
+    content: `Speak to one of our super-friendly, ultra-knowledgeable customer
+    service experts - we're on-call Monday - Friday 8:00 - 21:00, and
+    Saturday - Sunday 8:00 - 18:00. Call us on 020 7768 5000 (UK), 1 800
+    870 0741 (US), email on`,
+  }
+]
