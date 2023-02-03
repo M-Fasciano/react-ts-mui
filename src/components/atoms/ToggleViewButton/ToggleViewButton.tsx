4c612a103e72ed742a64de0bde72bd153a7e001a
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-02-03 15:21:34 +0000

    Toggle icon fix

--- src/components/atoms/ToggleViewButton/ToggleViewButton.tsx
+++ src/components/atoms/ToggleViewButton/ToggleViewButton.tsx
@@ -1,5 +1,5 @@
-import { GridViewIcon, ListViewIcon } from "../../Icons/Icons";
 import { ButtonProps } from "../../pages/ToggleLayout";
+import { IconViewList, IconViewModule } from "../Icons/action";
 import { StyledButtonWrapper } from "./ToggleViewButton.style";
 
 function ToggleViewButton(props: ButtonProps) {
@@ -8,10 +8,10 @@ function ToggleViewButton(props: ButtonProps) {
   return (
     <StyledButtonWrapper>
       <button onClick={handleToggle} className={selected ? "active" : ""}>
-        <GridViewIcon htmlColor={"#CCCFD9"} />
+        <IconViewModule height={24} width={24} />
       </button>
       <button onClick={handleToggle} className={selected ? "" : "active"}>
-        <ListViewIcon htmlColor={"#CCCFD9"} />
+        <IconViewList height={24} width={24} />
       </button>
     </StyledButtonWrapper>
   );
