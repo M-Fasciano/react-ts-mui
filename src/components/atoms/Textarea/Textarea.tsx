2ff899fd76f5502b46f2781f32be9d8bc64d7f80
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-03-16 08:50:11 +0000

    Textarea fix

--- src/components/atoms/Textarea/Textarea.tsx
+++ src/components/atoms/Textarea/Textarea.tsx
@@ -1,25 +1,22 @@
-import { TextareaAutosizeProps as MuiTextareaProps } from "@mui/material";
-import { StyledTextarea } from "./Textarea.style";
-
-export interface TextareaProps {
-  ref?: MuiTextareaProps["ref"];
-  maxRows?: MuiTextareaProps["maxRows"];
-  minRows?: MuiTextareaProps["minRows"];
-  defaultValue?: MuiTextareaProps["defaultValue"];
-  disabled?: MuiTextareaProps["disabled"];
-  error?: boolean;
-  className?: string;
+import { TextFieldProps as MuiTextFieldProps } from "@mui/material";
+import { StyledTextArea } from "./Textarea.style";
+export interface TextAreaProps {
+  ref?: MuiTextFieldProps["ref"];
+  name?: MuiTextFieldProps["name"];
+  id?: MuiTextFieldProps["id"];
+  onChange?: MuiTextFieldProps["onChange"];
+  error?: MuiTextFieldProps["error"];
+  helperText?: MuiTextFieldProps["helperText"];
+  value?: MuiTextFieldProps["value"];
+  defaultValue?: MuiTextFieldProps["defaultValue"];
+  disabled?: MuiTextFieldProps["disabled"];
+  multiline?: MuiTextFieldProps["multiline"];
+  placeholder?: MuiTextFieldProps["placeholder"];
+  minRows?: MuiTextFieldProps["minRows"];
 }
 
-function Textarea(props: TextareaProps) {
-  return (
-    <StyledTextarea
-      nonce={undefined}
-      onResize={undefined}
-      onResizeCapture={undefined}
-      {...props}
-    />
-  );
+function TextArea(props: TextAreaProps) {
+  return <StyledTextArea {...props} />;
 }
 
-export default Textarea;
+export default TextArea;
