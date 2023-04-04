9f45e067c0e3190c46eb577f87885f465dcd3876
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-04-04 18:24:30 +0100

    WIP Order card

---
+++ src/utils/orderStatus.ts
@@ -0,0 +1,34 @@
+import { theme } from "../themes/theme";
+
+const OrderStatuses: any = {
+  M: "Created",
+  A: "Pending Payment",
+  L: "In Process",
+  H: "Created",
+  D: "Shipped",
+  R: "Confirmed",
+  X: "Cancelled",
+  "1": "Delivered",
+  E: "Pending Prescription",
+};
+
+export interface StatusMap {
+  color: string;
+  label: string;
+}
+
+export const statusMap = (status: string): StatusMap => {
+  let statusColor = theme.palette.success.main;
+
+  switch (status) {
+    case "E":
+      // case 'H':
+      statusColor = theme.palette.error.main;
+      break;
+  }
+
+  return {
+    color: statusColor,
+    label: OrderStatuses[status],
+  };
+};
