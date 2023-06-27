410835e53ebe41a41de674efa9f357dd77b73389
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-06-27 07:46:29 +0100

    feat: use dynamic columns on video and login

--- src/components/pages/Video.tsx
+++ src/components/pages/Video.tsx
@@ -1,25 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import WistiaReactPlayer from "../atoms/WistiaReactPlayer";
 import WistiaPlayerEmbed from "../atoms/WistiaPlayerEmbed/WistiaPlayerEmbed";
+import DynamicColumns from "../layouts/DynamicColumns";
 import styled from "styled-components";
-import { useState } from "react";
 
 const StyledWrapper = styled("div")`
   display: flex;
   flex-direction: column;
-  width: 100%;
-
-  ${({ theme }) => theme.breakpoints.up("md")} {
-    flex-direction: row;
-  }
-
-  .container {
-    margin: 2rem;
-
-    ${({ theme }) => theme.breakpoints.up("md")} {
-      width: 50%;
-    }
-  }
+  gap: 2rem;
+  margin: 2rem;
 `;
 
 export type VideoProps = {
@@ -58,22 +47,24 @@ function Video() {
 
   return (
     <StyledWrapper>
-      <div className="container">
-        <h1>React player</h1>
-        <WistiaReactPlayer
-          videoId="bq6epni33s"
-          state={state}
-          handleOnStart={handleOnStart}
-          handleOnPlay={handleOnPlay}
-          handleOnPause={handleOnPause}
-          handleOnEnded={handleOnEnded}
-        />
-      </div>
+      <DynamicColumns columnWidths={[50, 50]}>
+        <>
+          <h1>React player</h1>
+          <WistiaReactPlayer
+            videoId="bq6epni33s"
+            state={state}
+            handleOnStart={handleOnStart}
+            handleOnPlay={handleOnPlay}
+            handleOnPause={handleOnPause}
+            handleOnEnded={handleOnEnded}
+          />
+        </>
 
-      <div className="container">
-        <h1>Embed player</h1>
-        <WistiaPlayerEmbed videoId="bq6epni33s" />
-      </div>
+        <>
+          <h1>Embed player</h1>
+          <WistiaPlayerEmbed videoId="bq6epni33s" />
+        </>
+      </DynamicColumns>
     </StyledWrapper>
   );
 }
